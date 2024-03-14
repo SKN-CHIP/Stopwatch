1be3a2c10243937257cd4d0c9adfaf17faaf5a9e
Patryk Fryza <94052985+PatrykFryza@users.noreply.github.com>
2024-03-14 17:40:24 +0100

    added pwm library

---
+++ Buzzer/Core/Inc/led_pwm.h
@@ -0,0 +1,19 @@
+/*
+ * led_pwm.h
+ *
+ *  Created on: Mar 14, 2024
+ *      Author: patry
+ */
+
+#ifndef INC_LED_PWM_H_
+#define INC_LED_PWM_H_
+
+#include "main.h"
+
+void enable_timer3(void);
+
+void generate_signal(uint8_t* data, uint32_t data_length);
+
+void dma_init(void);
+
+#endif /* INC_LED_PWM_H_ */
